bdb6f3cdde9ab9eb56accd1363b02563f40d154c
Barbara Gimeno <barbaragimeno.cm@gmail.com>
2023-10-22 18:23:32 +0200

    Add header with h1 with logo

--- src/App/App.ts
+++ src/App/App.ts
@@ -3,14 +3,17 @@ import Component from "../component/Components.js";
 class App extends Component {
   constructor(parentElement: Element) {
     super(parentElement, "div", "main-container");
-
-    parentElement.appendChild(this.element);
   }
 
   protected populate(): void {
     this.element.innerHTML = `
-    <header class= main-component>
-    </header>`;
+    <header>
+      <h1> Pokemon characters
+      </h1>
+      <img class="image_title" src="pokemon-logo.svg">
+    <header>
+      <ul class= main-component>
+      </ul>`;
   }
 }
 
